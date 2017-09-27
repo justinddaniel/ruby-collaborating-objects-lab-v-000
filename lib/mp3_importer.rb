47edcc037ab99413f7e82e69188fd0fbcda775fa
Justin Daniel <justinddaniel@gmail.com>
2017-09-27 17:19:37 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,11 @@
+class MP3Importer
+
+  attr_accessor :test_music_path, :files
+
+  def initialize(test_music_path)
+    @test_music_path = test_music_path
+    @files = []
+  end
+
+  def files
+    
--- lib/song.rb
+++ lib/song.rb
@@ -10,5 +10,5 @@ class Song
   def self.new_by_filename(file_name)
     Song.new(file_name)
   end
-  
+
 end
