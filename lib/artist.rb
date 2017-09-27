d9a77816cc16fb9d722f6f70fb48544895d40eb9
Justin Daniel <justinddaniel@gmail.com>
2017-09-27 16:07:36 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,17 @@
 class Artist
 
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
+  def add_song(song_name)
+    song = Song.new(song_name)
+    song.artist = self
+    @songs << song
+  end
 
 
 end
