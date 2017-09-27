aebd050af93dc17606638270b9cf6f1a5697ae9b
Justin Daniel <justinddaniel@gmail.com>
2017-09-27 16:27:57 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,7 +24,7 @@ class Artist
   end
 
   def self.find_or_create_by_name(artist_name)
-    if @@all.name.detect(artist_name) == nil 
+    if @@all.name.detect(artist_name) == nil
       Artist.new(artist_name)
     else
       @@all.name.detect(artist_name)
